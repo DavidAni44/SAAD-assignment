39aef866824cf1179beb92b8857bd56257b9ec9c
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2024-11-16 19:38:56 +0000

    Frontend loads properly

--- Application/app/frontend.py
+++ Application/app/frontend.py
@@ -6,11 +6,29 @@ frontend = Blueprint('frontend', __name__)
 
 API_BASE_URL = 'http://127.0.0.1:5000/api'
 
-@frontend.route('/')
-def index():
+@frontend.route('/BorrowMedia')
+def borrowMedia():
     response = requests.get('http://127.0.0.1:5000/api/media/test')
     dict_rep = response.json() if response.status_code == 200 else {
         "message" : "fail"
     }
     
-    return render_template('base.html', dict_rep=dict_rep)
+    return render_template('BorrowMedia.html', dict_rep=dict_rep)
+
+@frontend.route('/ManageSubscription')
+def ManageSubscription():
+    response = requests.get('http://127.0.0.1:5000/api/media/test')
+    dict_rep = response.json() if response.status_code == 200 else {
+        "message" : "fail"
+    }
+    
+    return render_template('ManageSubscription.html', dict_rep=dict_rep)
+
+@frontend.route('/EditUser.html')
+def EditUser():
+    response = requests.get('http://127.0.0.1:5000/api/media/test')
+    dict_rep = response.json() if response.status_code == 200 else {
+        "message" : "fail"
+    }
+    
+    return render_template('EditUser.html', dict_rep=dict_rep)
