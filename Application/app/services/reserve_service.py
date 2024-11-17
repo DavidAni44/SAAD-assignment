f6dae58ebae3d1d6a06dace3f78189a861c39b32
xclwright <xclwright@gmail.com>
2024-11-17 22:10:45 +0000

    borrow/reserve/subscription

--- Application/app/services/reserve_service.py
+++ Application/app/services/reserve_service.py
@@ -1,12 +1,31 @@
 from app.services.database import user_collection, media_collection, transaction_collection, branch_collection
 from flask import request, jsonify
 
+
 def reserve_media(user_id, media_id):
     media_id = str(media_id)
     user = user_collection.find_one({"_id": user_id})
-    if not user:
-        return jsonify({"error": "User not found"}), 404
+    if check_media_unavaliable(user,media_id):
+        add_to_reserved_media(user,media_id)
 
+    else:
+        return jsonify({"error": "Media not found in user's home branch"}), 400
+
+
+
+def add_to_reserved_media(user_id,media_id):
+    result = user_collection.update_one(
+        {"_id": user_id},
+        {"$addToSet": {"reserved_media": str(media_id)}} 
+    )
+    if result.modified_count > 0:
+        return jsonify({"message": "Media reserved successfully"}), 200
+    else:
+        return jsonify({"message": "Media was already reserved"}), 200
+
+
+
+def check_media_unavaliable(user,media_id):
     home_branch_id = user.get("branch_id")
     if not home_branch_id:
         return jsonify({"error": "User does not have a home branch assigned"}), 400
@@ -25,15 +44,4 @@ def reserve_media(user_id, media_id):
         }
     })
 
-    if branch:
-        result = user_collection.update_one(
-            {"_id": user_id},
-            {"$addToSet": {"reserved_media": str(media_id)}} 
-        )
-
-        if result.modified_count > 0:
-            return jsonify({"message": "Media reserved successfully"}), 200
-        else:
-            return jsonify({"message": "Media was already reserved"}), 200
-    else:
-        return jsonify({"error": "Media is not available or not found in user's home branch"}), 400
+    return branch
