14639fc1ed86bed48f1137b0f102539ebc63a2d8
DavidAni44 <82607483+DavidAni44@users.noreply.github.com>
2024-12-05 14:06:34 +0000

    edit subscription amount

--- Application/app/__init__.py
+++ Application/app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from .api import init_api
@@ -14,6 +15,11 @@ load_dotenv()
 def create_app():
     # Correct the path for static_folder
     app = Flask(__name__, template_folder='templates', static_folder='static')
+    CORS(app, resources={r"/*": {
+        "origins": "*",  # You can restrict this to specific origins if needed
+        "methods": ["GET", "POST", "DELETE", "OPTIONS"],  # Allowed methods
+        "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"]  # Allowed headers
+    }})
     init_api(app)
     app.register_blueprint(frontend)
 
